4d9c474e3aa8efcf64720d4f24bdffb4c4b5e98f
Sebastian Kasanzew <sebkasanzew@gmail.com>
2016-04-13 15:00:50 +0200

    Update signin

--- both/lib/at_config.js
+++ both/lib/at_config.js
@@ -1,8 +1,40 @@
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 import { Router } from 'meteor/iron:router';
 
+const postLogout = function () {
+  Router.go('/login');
+};
+
 AccountsTemplates.configure({
   defaultLayout: 'mainLayout',
+
+  // Behavior
+  enablePasswordChange: true,
+  forbidClientAccountCreation: true,
+  overrideLoginErrors: true,
+  sendVerificationEmail: false,
+  lowercaseUsername: false,
+  focusFirstInput: true,
+
+  // Redirects
+  homeRoutePath: '/',
+  redirectTimeout: 4000,
+
+  // Hooks
+  onLogoutHook: postLogout,
+  /*onSubmitHook: mySubmitFunc,
+  preSignUpHook: myPreSubmitFunc,
+  postSignUpHook: myPostSubmitFunc,*/
+
+  // Appearance
+  showForgotPasswordLink: true,
+
+  // Client-side Validation
+  continuousValidation: true,
+  negativeValidation: true,
+  positiveValidation: true,
+  negativeFeedback: true,
+  positiveFeedback: true,
 });
 
 AccountsTemplates.configureRoute('signIn', {
