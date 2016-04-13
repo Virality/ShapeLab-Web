076d5b95829ca401ec0b8d8489e203078eb821ed
Sebastian Kasanzew <sebkasanzew@gmail.com>
2016-04-13 17:37:53 +0200

    Add account field for testing

--- both/lib/at_config.js
+++ both/lib/at_config.js
@@ -5,12 +5,21 @@ const postLogout = function () {
   Router.go('/login');
 };
 
+AccountsTemplates.addField({
+  _id: 'unity_session_key',
+  type: 'text',
+  displayName: 'ShapeLab Session',
+  required: true,
+  minLength: 5,
+  maxLength: 5,
+});
+
 AccountsTemplates.configure({
   defaultLayout: 'mainLayout',
 
   // Behavior
   enablePasswordChange: true,
-  forbidClientAccountCreation: true,
+  forbidClientAccountCreation: false,
   overrideLoginErrors: true,
   sendVerificationEmail: false,
   lowercaseUsername: false,
